30ece867b3f02c17a24aeee0bd5419fd55a168f8
WPSrobotics0 <wpsrobotics_0@hotmail.com>
2026-03-07 14:07:05 -0500

    rand commit (Sam)

--- src/main/java/frc/robot/commands/RotTurretCommand.java
+++ src/main/java/frc/robot/commands/RotTurretCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import java.util.function.Supplier;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LimelightHelpers;
@@ -9,7 +10,8 @@ import frc.robot.subsystems.TurretSubsystem;
 public class RotTurretCommand extends Command {
    private TurretSubsystem m_turret;
    private double m_speed = 0.0;
-   private Supplier<Double> m_rightX;
+   //private Supplier<Double> m_rightX;
+   private double m_tid;
    
    public RotTurretCommand(TurretSubsystem Turret){
     addRequirements(Turret);
@@ -20,23 +22,47 @@ public class RotTurretCommand extends Command {
     //only runs once
     @Override
     public void initialize() {
-        m_turret.RotateTurret(m_rightX);
+        //m_turret.RotateTurret(m_rightX);
+         double tid = LimelightHelpers.getFiducialID("limelight");
+        m_tid=tid;
     }
 //everyt time it is ran
     @Override
   public void execute() {
+    m_tid = LimelightHelpers.getFiducialID("limelight");
     //m_turret.RotateTurret(m_rightX);
-    m_speed=m_turret.m_TurretEncoder.getPosition()/16.0;
-    double target = LimelightHelpers.getTX("limelight")/20.0;
-    double scaleSpeed=2.0;
-    m_speed=Math.abs(target-m_speed)/scaleSpeed;
-    if (m_speed>0.3){
-      m_speed=0.3;
+    double gearRatio=16.0;
+      if ((m_tid!=-1 && m_tid!=0)) {
+        m_speed=m_turret.m_TurretEncoder.getPosition()/gearRatio;
+        double target = LimelightHelpers.getTX("limelight")/20.0;
+
+        if((Math.abs(target-m_speed))>.05){
+          double scaleSpeed=2.0;
+          m_speed=Math.abs(target-m_speed)/scaleSpeed;
+
+          if (m_speed>0.3){
+            m_speed=0.3;
+          }
+
+          if(target<(m_turret.m_TurretEncoder.getPosition()/gearRatio)){
+            m_speed*=-1.0;
+          }
+          
+          m_turret.RotateTurret(()->m_speed);
+        }
+        else{
+          m_turret.RotateTurret(()->0.0);
+        }
+        SmartDashboard.putNumber("rot turret target pos", target);
+        SmartDashboard.putNumber("rot turret pos", (m_turret.m_TurretEncoder.getPosition()/gearRatio));
+        SmartDashboard.putNumber("rot turret speed", m_speed);
+
+        
     }
-    if(target<(m_turret.m_TurretEncoder.getPosition()/16.0)){
-      m_speed*=-1.0;
+    else{
+      m_turret.RotateTurret(()->0.0);
     }
-    m_turret.RotateTurret(()->m_speed);
+    
   }
    @Override
   public void end(boolean interrupted) {
